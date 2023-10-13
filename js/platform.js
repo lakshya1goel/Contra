4e0a7b1f39883a70ddceb2261e2716ebc37c015d
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-10-13 14:51:34 +0530

    fix issues in jump

--- js/platform.js
+++ js/platform.js
@@ -1,42 +1,35 @@
-import Enemy from "./enemy.js"
-let count=0;
+import Enemy from "./enemy.js";
+let count = 0;
 let cnt = 1;
 export default class Platform {
-    constructor(a, b, w, h,c) {
-        this.position = {
-            x: a,
-            y: b
-        };
-        this.width = w;
-        this.height = h;
-        this.enemy;
-        if(c==0)
-        {
-            this.enemyX=2820;
-            this.enemyY=100;
-        }
-        else
-        {
-            this.enemyX=this.position.x;
-        this.enemyY=this.position.y-40-this.height;
-        }
-        
+  constructor(a, b, w, h, c) {
+    this.position = {
+      x: a,
+      y: b,
+    };
+    this.width = w;
+    this.height = h;
+    this.enemy;
+    if (c == 0) {
+      this.enemyX = 2820;
+      this.enemyY = 100;
+    } else {
+      this.enemyX = this.position.x;
+      this.enemyY = this.position.y - 40 - this.height;
+    }
+  }
+  draw(ctx) {
+    ctx.globalAlpha = 0;
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.globalAlpha = 1;
+  }
 
+  enemyAttack() {
+    this.enemy = new Enemy(this.enemyX, this.enemyY, 100, 100);
+  }
+  updateEnemies(ctx) {
+    if (this.enemy) {
+      this.enemy.update(ctx);
     }
-    draw(ctx) {
-        ctx.globalAlpha = 0;
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-        ctx.globalAlpha = 1; 
-    }
-     
-    enemyAttack(){
-            this.enemy=new Enemy(this.enemyX,this.enemyY,100, 100);
-    }
-    updateEnemies(ctx){
-        
-            if (this.enemy) {
-                this.enemy.update(ctx)
-            }
-    }
-    
+  }
 }
